5219cb797618701d67d7e77468414c79402b0caa
Darell Rangga <darrelrangga@gmail.com>
2025-06-12 13:36:12 +0700

    Add title attribute to Google Maps iframe for better accessibility; remove unused review slider code

--- js/script.js
+++ js/script.js
@@ -95,32 +95,6 @@ var swiper = new Swiper(".featured-slider", {
   },
 });
 
-var swiper = new Swiper(".review-slider", {
-  grabCursor: true,
-  centeredSlides: true,
-  spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
-
 // MODAL BOX
 document.addEventListener("DOMContentLoaded", function () {
   var motorDetailModal = document.getElementById("motorDetailModal");
