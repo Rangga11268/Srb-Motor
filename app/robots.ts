b27b32593e6cf758286cef61750b6d93c44c78a1
Darell Rangga <darrelrangga@gmail.com>
2026-06-16 14:37:15 +0700

    Revert "chore: correct website deployment domain from srb-motor to srb-motors"
    This reverts commit 0c04ccebaea7a2f71beb0f06491661a5bf0806b3.

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://srb-motors.vercel.app/sitemap.xml",
+    sitemap: "https://srb-motor.vercel.app/sitemap.xml",
   };
 }
